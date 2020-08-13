2acb6015f7e038946dea6225e99ea2ac61026ad0
Morgan <jmorgansmith88@gmail.com>
2020-08-12 21:33:12 -0400

    updates

--- app.js
+++ app.js
@@ -1,7 +1,58 @@
     //Budget Controller
     var budgetController = (function() {
         
-        //some code
+        var Expense = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+
+        var Income = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+
+        var data = {
+            allItems: {
+                exp: [],
+                inc: []
+            },
+            totals: {
+                exp: 0,
+                inc: 0
+            }
+        };
+
+        return {
+            addItem: function(type, des, val) {
+                var newItem, ID;
+                console.log(type);
+                //Create new ID
+                if (data.allItems[type].length > 0) {
+                    ID = data.allItems[type][data.allItems[type].length -1 ].id + 1;
+                }else {
+                    ID = 0;
+                }
+
+                //Create new item based on 'inc' or 'exp' type
+                if (type === 'exp') {
+                    newItem = new Expense(ID, des, val);
+                } else if(type === 'inc') {
+                    newItem = new Income(ID, des, val);
+                }
+
+                //push to data structure
+                data.allItems[type].push(newItem);
+
+                //return new element
+                return newItem;
+            },
+
+            testing: function() {
+                console.log(data);
+            }
+        }
 
     })();
 
@@ -9,37 +60,67 @@
     //UI Controller
     var UIController = (function() {
 
+        var DOMstrings = {
+            inputType: '.add__type',
+            inputDescription: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+        };
+
         return {
             getInput: function() {
-                var type = document.querySelector('.add__type').value;
+                return{
+                    type: document.querySelector(DOMstrings.inputType).value, // Will be either income or expense
+                    description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value,
+                };
+            },
+
+            getDOMstrings: function() {
+                return DOMstrings;
             }
-        }
+        };
 
     })();
 
     //Global App Controller
     var appController = (function (budgetCtrl, UICtrl) {
 
-        var ctrlAddItem = function () {
-            // 1. Get the filed input data
+        var setupEventListeners = function() {
+            
+            var DOM = UICtrl.getDOMstrings();
 
+            document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+            document.addEventListener('keypress', function(event) {
+                if (event.keyCode === 13 || event.which === 13) {
+                   ctrlAddItem();
+                }
+            });
+        }
+
+        var ctrlAddItem = function () {
+            var input, newItem;
+
+            // 1. Get the filed input data
+            input = UICtrl.getInput();
             // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
             // 3. add the new item to the user interface
 
             // 4. Calculate the budget
 
             // 5. Display the budget on the UI
-            console.log('It works!');
-        }
+        };
 
-
-        document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-        document.addEventListener('keypress', function(event) {
-            if (event.keyCode === 13 || event.which === 13) {
-                ctrlAddItem();
+        return {
+            init: function() {
+                console.log('Application has started.');
+                setupEventListeners();
             }
-        })
+        };
 
     })(budgetController, UIController);
+
+    appController.init();
